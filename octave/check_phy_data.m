afa4570b638bba7900c2882d72545ac47ef94c1d
Rainer Rudigier <rainer@rudigier.at>
2023-01-21 23:53:11 +0100

    STS sync checks

--- octave/check_phy_data.m
+++ octave/check_phy_data.m
@@ -16,21 +16,25 @@ s_hat_abs = []
 tau_hat = []
 
 for i = 1:n
-  s_hat_abs(end+1) = IQ{i,2};
-  s_hat_arg(end+1) = IQ{i,3};
-  tau_hat(end+1) = IQ{i,4};
+  s_hat_abs(end+1) = IQ{i,3};
+  s_hat_arg(end+1) = IQ{i,4};
+  tau_hat(end+1) = IQ{i,5};
 endfor
 
-figure
-subplot(2,1,1)
-plot(1:n,s_hat_abs(1:n))
-subplot(2,1,2)
-plot(1:n,s_hat_arg(1:n))
+#figure
+#subplot(2,1,1)
+#plot(1:n,s_hat_abs(1:n))
+#subplot(2,1,2)
+#plot(1:n,s_hat_arg(1:n))
 
 figure
 subplot(2,1,1)
 plot(1:n,s_hat_abs)
+ylabel('s hat')
+xlabel('STS sync hit')
 subplot(2,1,2)
 plot(1:n,tau_hat)
+ylabel('tau prime')
+xlabel('STS sync hit')
 
 
